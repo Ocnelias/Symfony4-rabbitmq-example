e933e2bed26e9513176bfe1f056da12b5fe6df2b
Dmytro Salenko <dmytro.salenko@pinxterapp.com>
2019-11-23 19:12:01 +0200

    sender controller

--- src/Controller/SenderController.php
+++ src/Controller/SenderController.php
@@ -3,14 +3,29 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use OldSound\RabbitMqBundle\OldSoundRabbitMqBundle;
 
-class SenderController extends AbstractController
+class SenderController extends Controller
 {
 
-    public function indexAction()
+    /**
+     * @Route("/create_user", name="create_user", methods={"GET"})
+     */
+    public function postCreateUser(Request $request)
     {
 
-    }
+        $message = ["Type"=>"VerificationEmail","Firstname"=>$request->get('firstname'),"Lastname"=>$request->get('lastname'),"Email"=>$request->get('email')];
+        $rabbitMessage = json_encode($message);
 
+
+        $this->get('old_sound_rabbit_mq.emailing_producer')->setContentType('application/json');
+        $this->get('old_sound_rabbit_mq.emailing_producer')->publish($rabbitMessage);
+
+        return new JsonResponse(array('Status' => 'OK'));
+
+    }
 }
